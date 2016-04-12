792649a9e85a67e112b9b2d26e1d4169fa11941e
Manos Tsardoulias <etsardou@gmail.com>
2016-04-12 11:27:18 +0300

    Added error checking in humanoid motion

--- python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_audio.py
+++ python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_audio.py
@@ -22,6 +22,11 @@ class DeviceAudio(Audio):
         self.speech_recog = ALProxy("ALSpeechRecognition", self.nao_ip, self.nao_port)
         self.memory = ALProxy("ALMemory", self.nao_ip, self.nao_port)
 
+    # Assistive function to return errors
+    def ret_exc(self, text):
+        print text
+        return [None, text]
+
     # Call to make NAO dictate a string. English is the default language.
     def speak(self, text, language = 'English'):
         # Sanity conversion
@@ -31,14 +36,13 @@ class DeviceAudio(Audio):
         supp_lang = ['French', 'Chinese', 'English', 'German', 'Italian',\
                 'Japanese', 'Korean', 'Portuguese', 'Spanish', 'Greek']
         if language not in supp_lang:
-            return [None, 'audio.speak: Unsupported language']
+            self.ret_exc('audio.speak: Unsupported language')
         try:
             self.tts.setLanguage(language)
             self.tts.say(_text)
         except Exception as e:
-            error = "audio.speak: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
+            self.ret_exc("audio.speak: Unrecognized exception: " + e.message)
+        
         return [None, None]
 
     # Call to make NAO start a recording. Supported audio types are wav (1-ch and
@@ -51,21 +55,13 @@ class DeviceAudio(Audio):
             channels = [0, 0, 1, 0]):
 
         if filename == '':
-            error = "audio.startRecording: Invalid filename"
-            print error
-            return [None, error]
+            self.ret_exc("audio.startRecording: Invalid filename")
         if audio_type not in ['ogg', 'wav']:
-            error = "audio.startRecording: Unsupported audio type"
-            print error
-            return [None, error]
+            self.ret_exc("audio.startRecording: Unsupported audio type")
         if samplerate <= 0 or not isinstance(samplerate, int):
-            error = "audio.startRecording: Unsupported samplerate"
-            print error
-            return [None, error]
+            self.ret_exc("audio.startRecording: Unsupported samplerate")
         if type(channels) is not list:
-            error = "audio.startRecording: Not valid channels format"
-            print error
-            return [None, error]
+            self.ret_exc("audio.startRecording: Not valid channels format")
 
         # Channels copy
         inner_channels = channels[:]
@@ -85,9 +81,7 @@ class DeviceAudio(Audio):
                 samplerate, inner_channels)
         except Exception as e:
             self.audio_rec.stopMicrophonesRecording()
-            error = "audio.startRecording: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
+            self.ret_exc("audio.startRecording: Unrecognized exception: " + e.message)
 
         return [None, None]
 
@@ -96,9 +90,7 @@ class DeviceAudio(Audio):
         try:
             self.audio_rec.stopMicrophonesRecording()
         except Exception as e:
-            error = "audio.stopRecording: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
+            self.ret_exc("audio.stopRecording: Unrecognized exception: " + e.message)
         return [None, None]
 
     # Plays a file stored in NAO. Filanema path must be absolute.
@@ -106,24 +98,18 @@ class DeviceAudio(Audio):
         try:
             self.audio_player.playFile(filename)
         except Exception as e:
-            error = "audio.playFile: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
+            self.ret_exc("audio.playFile: Unrecognized exception: " + e.message)
         return [None, None]
 
     # Sets the NAO volume. The volume must be between 0 and 100, integer.
     def setVolume(self, volume):
         if type(volume) is not int or volume < 0 or volume > 100:
-            error = "audio.setVolume: Wrong volume format or value"
-            print error
-            return [None, error]
+            self.ret_exc("audio.setVolume: Wrong volume format or value")
 
         try:
             self.audio_device.setOutputVolume(volume)
         except Exception as e:
-            error = "audio.setVolume: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
+            self.ret_exc("audio.setVolume: Unrecognized exception: " + e.message)
 
         return [None, None]
 
@@ -139,15 +125,14 @@ class DeviceAudio(Audio):
         [ret, err] = \
                 self.startRecording(filename, audio_type, samplerate, channels)
         if err != None:
-            print err
-            return [None, err]
+            self.ret_exc('audio.record: ' + err)
 
         time.sleep(seconds)
 
         [ret, err] = self.stopRecording()
         if err != None:
-            print err
-            return [None, err]
+            self.ret_exc('audio.record: ' + err)
+        
         return [None, None]
 
     # Performs speech detection with the NAO engine. 
@@ -158,35 +143,25 @@ class DeviceAudio(Audio):
 
         # Sanity checks
         if type(vocabulary) is not list:
-            error = 'audio.speechDetection: Vocabulary not a list'
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Vocabulary not a list')
         if type(wait) is not float and type(wait) is not int:
-            error = 'audio.speechDetection: Wait param not a number'
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Wait param not a number')
         if wait <= 0:
-            error = 'audio.speechDetection: Wait param negative value'
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Wait param negative value')
 
         # Setting language and activating the speech recognition
         try:
             self.speech_recog.setLanguage(language)
             self.speech_recog.setVocabulary(vocabulary, False)
         except Exception as e:
-            error = "audio.speechDetection: Unrecognized exception: " + e.message
-            print error
-            return [None, error]
-        
+            self.ret_exc("audio.speechDetection: Unrecognized exception: " + e.message)
+
         # Subscribing to the speech recognition event
         try:
             self.speech_recog.subscribe("rapp_speech_rec")
         except Exception as e:
-            error = 'audio.speechDetection: Not able to subscribe to speech \
-                    recognition system: ' + e.message
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Not able to subscribe to speech \
+                    recognition system: ' + e.message)
 
         # Wait for 'wait' seconds
         try:
@@ -195,25 +170,20 @@ class DeviceAudio(Audio):
                 time.sleep(0.1)
                 iterations -= 1
         except Exception as e:
-            error = 'audio.speechDetection: Something wrong with NAO memory proxy: ' +\
-                    e.message
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Something wrong with NAO memory proxy: ' +\
+                    e.message)
 
         # If no speech detected return error
         if iterations <= 0:
-            error = "audio.speechDetection: Timeout without speech detection"
-            return [None, error]
+            self.ret_exc("audio.speechDetection: Timeout without speech detection")
 
         # Read the recognized words and return them
         try:
             [word, probability] = self.memory.getData("WordRecognized")
             self.speech_recog.unsubscribe("rapp_speech_rec")
         except Exception as e:
-            error = 'audio.speechDetection: Error in unsubscription from speech \
-                    recognition: ' + e.message
-            print error
-            return [None, error]
+            self.ret_exc('audio.speechDetection: Error in unsubscription from speech \
+                    recognition: ' + e.message)
 
         return [[word, probability], None]
 
--- python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_humanoid_motion.py
+++ python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_humanoid_motion.py
@@ -4,8 +4,10 @@ from rapp_robot_api_humanoid_motion import HumanoidMotion
 
 from naoqi import ALProxy
 
+# Handles the humanoid-specific motion calls
 class DeviceHumanoidMotion(HumanoidMotion):
-
+    
+    # Initialization of NAO proxies
     def __init__(self, parameters):
 
         self.nao_ip = parameters["nao_ip"]
@@ -14,44 +16,114 @@ class DeviceHumanoidMotion(HumanoidMotion):
         self.posture = ALProxy("ALRobotPosture", self.nao_ip, self.nao_port)
         self.motion = ALProxy("ALMotion", self.nao_ip, self.nao_port)
 
+        self.allowed_joints = ['HeadYaw', 'LShoulderPitch', 'LHipYawPitch1', \
+                'RHipYawPitch1', 'RShoulderPitch', 'HeadPitch', 'LShoulderRoll', \
+                'LHipRoll', 'RHipRoll', 'RShoulderRoll', \
+                'LElbowYaw', 'LHipPitch', 'RHipPitch', 'RElbowYaw', \
+                'LElbowRoll', 'LKneePitch', 'RKneePitch', 'RElbowRoll', \
+                'LWristYaw2', 'LAnklePitch', 'RAnklePitch', 'RWristYaw2', \
+                'LHand2', 'RAnkleRoll', 'LAnkleRoll', 'RHand2']
+
+    # Assistive function to return errors
+    def ret_exc(self, text):
+        print text
+        return [None, text]
+
+    # Sets a humanoid robot's joints. 'joints' are the names of the joints.
+    # The nonmeclature used is the one of the NAO robot:
+    # http://doc.aldebaran.com/2-1/family/robots/bodyparts.html#nao-chains
+    # Angles are in rads.
+    # Speed must be between [0,1], where 1 is the maximum nominal speed
     def setJointAngles(self, joints, angles, speed):
-        self.motion.setAngles(joints, angles, speed)
+        
+        for j in joints:
+            if j not in self.allowed_joints:
+                self.ret_exc('hunanoid_motion.setJointAngles: Not a valid \
+                        joint: ' + str(j))
+        if speed <= 0 or speed > 1:
+            self.ret_exc('humanoid_motion.setJointAngles: Speed out of bounds')
+
+        try:
+            self.motion.setAngles(joints, angles, speed)
+        except Exception as e:
+            self.ret_exc("humanoid_motion.setJointAngles: Unrecognized exception: "\
+                    + e.message)
+
         return [None, None]
 
+    # Returns the joint angles of a humanoid. Again 'joints' are the joints'
+    # names, which can be found here:
+    # http://doc.aldebaran.com/2-1/family/robots/bodyparts.html#nao-chains
     def getJointAngles(self, joints):
-        data = self.motion.getAngles(joints, False) 
+        for i in joints:
+            if i not in self.allowed_joints:
+                self.ret_exc('hunanoid_motion.getJointAngles: Not a valid \
+                        joint: ' + str(j))
+
+        try:
+            data = self.motion.getAngles(joints, False) 
+        except Exception as e:
+            self.ret_exc("humanoid_motion.setJointAngles: Unrecognized exception: "\
+                    + e.message)
+
         return [data, None]
 
+    # Opens a hand of a humanoid robot. The input must be 'Right' or 'Left'
     def openHand(self, hand_name):
-        if hand_name == "Right":
-            self.motion.openHand('RHand')
-        elif hand_name == "Left":
-            self.motion.openHand('LHand')
-        else:
-            return [None, "Error in hand name"]
+        if hand_name not in ['Right', 'Left']:
+            self.ret_exc('humanoid_motion.openHand: Unsupported hand name: ' +\
+                    hand_name)
+        try:
+            if hand_name == "Right":
+                self.motion.openHand('RHand')
+            elif hand_name == "Left":
+                self.motion.openHand('LHand')
+        except Exception as e:
+            self.ret_exc("humanoid_motion.openHand: Unrecognized exception: "\
+                    + e.message)
+
         return [None, None]
 
+    # Closes a NAO's hand. The input must be 'Right' or 'Left'
     def closeHand(self, hand_name):
+        if hand_name not in ['Right', 'Left']:
+            self.ret_exc('humanoid_motion.closeHand: Unsupported hand name: ' +\
+                    hand_name)
+
         if hand_name == "Right":
             self.motion.closeHand('RHand')
         elif hand_name == "Left":
             self.motion.closeHand('LHand')
-        else:
-            return [None, "Error in hand name"]
+
         return [None, None]
 
+    # Forces NAO to go to a predefined posture. The supported postures are here:
+    # http://doc.aldebaran.com/2-1/naoqi/motion/alrobotposture.html#term-predefined-postures
     def goToPosture(self, posture, speed):
         if posture not in ["Crouch", "LyingBack", "LyingBelly", "Sit", "SitRelax",\
                 "Stand", "StandInit", "StandZero"]:
-            return [None, "Not supported posture"]
+            self.ret_exc("humanoid_motion.goToPosture: Not supported posture: " +\
+                    posture]
 
         if speed <= 0.0 or speed > 1.0:
-            return [None, "Speed out of bounds"]
+            self.ret_exc('humanoid_motion.goToPosture: Speed out of bounds')
 
-        self.posture.goToPosture(posture, speed)
+        try:
+            self.posture.goToPosture(posture, speed)
+        except Exception as e:
+            self.ret_exc("humanoid_motion.goToPosture: Unrecognized exception: "\
+                    + e.message)
+
         return [None, None]
 
+    # Returns the NAO's posture. If the robot has no predefined posture it will
+    # return 'unknown'
     def getPosture(self):
-        ans = self.posture.getPosture()
-        return [ans, None]
+        try:
+            ans = self.posture.getPosture()
+            return [ans, None]
+        except Exception as e:
+            self.ret_exc("humanoid_motion.getPosture: Unrecognized exception: "\
+                    + e.message)
+
 
