13ad29b0b6a4a19274dbb232e65d71071acc2114
Manos Tsardoulias <etsardou@gmail.com>
2016-04-13 11:18:09 +0300

    Added error handling in nao sensors

--- python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_sensors.py
+++ python/implementations/nao_v4_naoqi2.1.4/rapp_nao_api_sensors.py
@@ -6,8 +6,10 @@ from rapp_robot_api_sensors import Sensors
 
 from naoqi import ALProxy
 
+# Handles the NAO sensors input
 class DeviceSensors(Sensors):
 
+    # Initialization of NAO procies
     def __init__(self, parameters):
 
         self.nao_ip = parameters["nao_ip"]
@@ -19,18 +21,45 @@ class DeviceSensors(Sensors):
         self.sonars = ALProxy("ALSonar", self.nao_ip, self.nao_port)
         self.sonars.subscribe("rapp_sonars_subscriber")
 
-    def getBatteryLevels(self): 
-        level = self.battery.getBatteryCharge()
-        return [[level], None]
+    # Assistive function to return errors
+    def ret_exc(self, text):
+        print text
+        return [None, text]
 
+    # Returns the battery levels in percentage form
+    def getBatteryLevels(self): 
+        try:
+            level = self.battery.getBatteryCharge()
+            return [[level], None]
+        except Exception as e:
+            self.ret_exc("sensors.getBatteryCharge: Unrecognized exception: " + \
+                e.message)
+ 
+    # Returns the two sonars measurements in meters. The result is a python
+    # dictionaryu containing 'front_left' and 'front_right'
     def getSonarsMeasurements(self):
-        left = self.memory.getData("Device/SubDeviceList/US/Left/Sensor/Value")
-        right = self.memory.getData("Device/SubDeviceList/US/Right/Sensor/Value")
+        try:
+            left = self.memory.getData("Device/SubDeviceList/US/Left/Sensor/Value")
+            right = self.memory.getData("Device/SubDeviceList/US/Right/Sensor/Value")
+        except Exception as e:
+            self.ret_exc("sensors.getSonarsMeasurements: Unrecognized exception: " + \
+                e.message)
+
         data = {}
         data['front_left'] = left
         data['front_right'] = right
         return [data, None]
 
+    # Returns the NAO tactiles and bumpers readings. Since this is an event-based
+    # call, the user must provide a delay of 'wait' seconds
+    # The result is a python dictionary including the following keys:
+    # 'head_front', 'head_rear', 'head_middle', 'left_hand_left', 'left_hand_right'
+    # 'left_hand_back', 'right_hand_back', 'right_hand_right', 'right_hand_left'
+    # 'left_foot', 'right_foot'
+    # The 'get_history' input variable is by default False. When False the 
+    # result is just a dictionary containing the aforementioned values. When
+    # it is True the returning value is a list of dictionaries containing
+    # a snapshot of measurements every 0.1 seconds.
     def getTactileMeasurements(self, wait, get_history = False):
         data = {}
         data['head_front'] = 0
@@ -49,24 +78,45 @@ class DeviceSensors(Sensors):
         data['right_foot'] = 0
 
         ret = []
+
+        if wait < 0.0:
+            self.ret_exc('sensors.getTactileMeasurements: Waiting value is \
+                negative')
+
         iterations = wait / 0.1
         while iterations > 0:
             time.sleep(0.1)
             iterations -= 1
-            hef = self.memory.getData("Device/SubDeviceList/Head/Touch/Front/Sensor/Value")
-            her = self.memory.getData("Device/SubDeviceList/Head/Touch/Rear/Sensor/Value")
-            hem = self.memory.getData("Device/SubDeviceList/Head/Touch/Middle/Sensor/Value")
 
-            lhb = self.memory.getData("Device/SubDeviceList/LHand/Touch/Back/Sensor/Value")
-            lhl = self.memory.getData("Device/SubDeviceList/LHand/Touch/Left/Sensor/Value")
-            lhr = self.memory.getData("Device/SubDeviceList/LHand/Touch/Right/Sensor/Value")
+            try:
+                hef = self.memory.getData(\
+                    "Device/SubDeviceList/Head/Touch/Front/Sensor/Value")
+                her = self.memory.getData(\
+                    "Device/SubDeviceList/Head/Touch/Rear/Sensor/Value")
+                hem = self.memory.getData(\
+                    "Device/SubDeviceList/Head/Touch/Middle/Sensor/Value")
 
-            rhb = self.memory.getData("Device/SubDeviceList/RHand/Touch/Back/Sensor/Value")
-            rhl = self.memory.getData("Device/SubDeviceList/RHand/Touch/Left/Sensor/Value")
-            rhr = self.memory.getData("Device/SubDeviceList/RHand/Touch/Right/Sensor/Value")
+                lhb = self.memory.getData(\
+                    "Device/SubDeviceList/LHand/Touch/Back/Sensor/Value")
+                lhl = self.memory.getData(\
+                    "Device/SubDeviceList/LHand/Touch/Left/Sensor/Value")
+                lhr = self.memory.getData(\
+                    "Device/SubDeviceList/LHand/Touch/Right/Sensor/Value")
 
-            fl = self.memory.getData("Device/SubDeviceList/LFoot/Bumper/Left/Sensor/Value")
-            fr = self.memory.getData("Device/SubDeviceList/RFoot/Bumper/Right/Sensor/Value")
+                rhb = self.memory.getData(\
+                    "Device/SubDeviceList/RHand/Touch/Back/Sensor/Value")
+                rhl = self.memory.getData(\
+                    "Device/SubDeviceList/RHand/Touch/Left/Sensor/Value")
+                rhr = self.memory.getData(\
+                    "Device/SubDeviceList/RHand/Touch/Right/Sensor/Value")
+
+                fl = self.memory.getData(\
+                    "Device/SubDeviceList/LFoot/Bumper/Left/Sensor/Value")
+                fr = self.memory.getData(\
+                    "Device/SubDeviceList/RFoot/Bumper/Right/Sensor/Value")
+            except Exception as e:
+                self.ret_exc("sensors.getTactileMeasurements: Unrecognized \
+                    exception: " + e.message)
 
             if get_history:
                 data['head_front'] = int(hef)
