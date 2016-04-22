b47a1edb8cf47654408253018ad0b15bdf35dc78
maciek-slon <maciek.slon@gmail.com>
2016-04-22 18:11:41 +0200

    Catkinize package

--- cpp/includes/rapp-robots-api/vision/vision.hpp
+++ cpp/includes/rapp-robots-api/vision/vision.hpp
@@ -85,7 +85,7 @@ public:
      *
      * \return QR-codes informations
      */
-    rapp::object::QRCode3D qr_code_detection(rapp::object::picture image, std::vector<std::vector<float>> robot_to_camera_matrix, double camera_matrix[][3], float landmark_theoretical_size = 0.16f);
+    rapp::object::QRCode3D qr_code_detection(rapp::object::picture::Ptr image, std::vector<std::vector<float>> robot_to_camera_matrix, double camera_matrix[][3], float landmark_theoretical_size = 0.16f);
 
 private:
     /**
