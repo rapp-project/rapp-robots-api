33332bf9fa667537e37931deb2de03df92c9bea2
maciek-slon <maciek.slon@gmail.com>
2016-05-17 15:31:47 +0200

    Add fake_image flag for vision library

--- cpp/implementations/info.cpp
+++ cpp/implementations/info.cpp
@@ -19,7 +19,7 @@ info::info(int argc, char * argv[])
     ;
 
     po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::store(po::command_line_parser(argc, argv).options(desc).allow_unregistered().run(), vm);
     po::notify(vm);
 
     if (vm.count("base_path")) {
--- cpp/implementations/vision.cpp
+++ cpp/implementations/vision.cpp
@@ -1,29 +1,66 @@
 #include <iostream>
+#include <stdexcept>
 
 #include <rapp-robots-api/vision/vision.hpp>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <boost/filesystem.hpp>
+namespace fs = boost::filesystem;
+
 
 namespace rapp {
 namespace robot {
 
 // placeholder class for vision implementation
 class VisionImpl {
+public:
+    std::string fake_image_;
 };
 
 vision::vision(int argc, char * argv[]) : pimpl(NULL) 
 {
+    pimpl = new VisionImpl;
     std::cout << "Initialized placeholder rapp::robot::vision library" << std::endl;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("fake_image", po::value<std::string>(), "fake iamge")
+    ;
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(desc).allow_unregistered().run(), vm);
+    po::notify(vm);
+
+    if (vm.count("fake_image")) {
+        pimpl->fake_image_ = vm["fake_image"].as<std::string>();
+        std::cout << "Using fake image: " << pimpl->fake_image_ << "\n";
+
+        fs::path p = pimpl->fake_image_;
+        if (!fs::exists(p)) {
+            throw std::runtime_error("rapp::robot::info: base path " + pimpl->fake_image_ + " doesn't exist!");
+        }
+    }
 }
 
 vision::~vision() 
 {
     std::cout << "Finished placeholder rapp::robot::vision library" << std::endl;
+    delete pimpl;
 }
 
 rapp::object::picture::Ptr vision::capture_image (int camera_id, int camera_resolution, const std::string & encoding) 
 {
     std::cout << "vision::captureImage" << std::endl;
-	rapp::types::byte bytes[2] = {0x89, 0x50};
-    return std::make_shared<rapp::object::picture>(std::vector<rapp::types::byte>(bytes, bytes+2));
+    if (pimpl->fake_image_ != "") {
+        std::cout << "returning " << pimpl->fake_image_ << "\n";
+        auto pic = std::make_shared<rapp::object::picture>(pimpl->fake_image_);
+        std::cout << "Type: " << pic->type() << "\n";
+        return pic;
+    } else {
+        rapp::types::byte bytes[2] = {0x89, 0x50};
+        return std::make_shared<rapp::object::picture>(std::vector<rapp::types::byte>(bytes, bytes+2));
+    }
 }
 
 bool vision::set_camera_param(int camera_id, int parameter_id, int new_value) 
