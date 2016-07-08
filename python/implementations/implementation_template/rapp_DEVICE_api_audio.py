9630e35bd410d271c5317cc4c85754461b987a1c
Aris Thallas <aris.thallas@gmail.com>
2016-07-08 11:44:54 +0300

    Fixed template format. (closes #19)

--- python/implementations/implementation_template/rapp_DEVICE_api_audio.py
+++ python/implementations/implementation_template/rapp_DEVICE_api_audio.py
@@ -7,23 +7,23 @@ class DeviceAudio(Audio):
     def __init__(self, parameters):
         pass
 
-    def speak(self, text, language): 
-        return {'response': '', 'error': 'Not implemented yet'}
+    def speak(self, text, language):
+        return {'error': 'Not implemented yet'}
 
-    def startRecording(self, filename, audio_type, samplerate, channels): 
-        return {'response': '', 'error': 'Not implemented yet'}
+    def startRecording(self, filename, audio_type, samplerate, channels):
+        return {'error': 'Not implemented yet'}
 
     def stopRecording(self):
-        return {'response': '', 'error': 'Not implemented yet'}
+        return {'error': 'Not implemented yet'}
 
     def playFile(self, filename):
-        return {'response': '', 'error': 'Not implemented yet'}
+        return {'error': 'Not implemented yet'}
 
     def setVolume(self, volume):
-        return {'response': '', 'error': 'Not implemented yet'}
+        return {'error': 'Not implemented yet'}
 
     def record(self, filename, seconds, audio_type, samplerate, channels):
-        return {'response': '', 'error': 'Not implemented yet'}
+        return {'error': 'Not implemented yet'}
 
     def speechDetection(self, vocabulary, wait, language):
-        return {'response': '', 'error': 'Not implemented yet'}
+        return {'error': 'Not implemented yet'}
