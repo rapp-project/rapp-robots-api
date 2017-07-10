89bd545d94cf799353f0946f64eb92234e4551af
alexampa <alexampa@ece.auth.gr>
2017-07-10 18:35:22 +0300

    Update posture_examples.py

--- python/examples/posture_examples.py
+++ python/examples/posture_examples.py
@@ -5,7 +5,7 @@ from rapp_robot_api import RappRobot
 
 rh = RappRobot()
 
-[data, err] = rh.humanoid_motion.getPosture()
+[err, data] = rh.humanoid_motion.getPosture().values()
 rh.audio.speak("My posture is " + data)
 
 rh.humanoid_motion.goToPosture("Stand", 0.7)
@@ -13,7 +13,7 @@ rh.humanoid_motion.goToPosture("Stand", 0.7)
 time.sleep(3)
 
 rh.audio.speak("My posture is " + \
-        rh.humanoid_motion.getPosture()[0])
+        rh.humanoid_motion.getPosture().values()[1])
 rh.humanoid_motion.goToPosture("Sit", 0.7)
 
 rh.motion.disableMotors()
